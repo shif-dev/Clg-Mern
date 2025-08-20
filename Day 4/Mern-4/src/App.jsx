a030dd27e163b6a69b836b3cd82d69858fcb4052
shifan-mts <amohamedshifan1@gmail.com>
2025-08-20 06:08:43 +0530

    day 4

---
+++ Day 4/Mern-4/src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+  <nav>
+  <h2>ToolM5</h2>
+  <div>
+    <a href="#">About Us</a>
+    <a href="#">Services</a>
+    <a href="#">Contacts</a>
+    <button  className="get" >Get Started</button>
+  </div>
+</nav>
+
+<div className="hero">
+  <h1>Digital Art</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quo placeat natus assumenda quod illo quaerat ipsum iure dolorum. Quos, perspiciatis consectetur expedita nulla, facilis dolor totam omnis asperiores voluptatibus in, veritatis quis inventore aut ad dolorum consequuntur corrupti? Tenetur dolorem impedit dolores ipsa earum praesentium itaque consectetur totam soluta.</p>
+  <button>View More </button>
+  <button className="get">Get Started </button>
+</div>
+
+<div class="about">
+  <div>
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quasi placeat ut, voluptates eum totam aperiam. Maxime, ullam? Aspernatur magni doloremque a animi sapiente molestias tempora. Labore nisi, impedit debitis exercitationem voluptatem commodi illum quisquam eius iusto dolor saepe molestiae beatae similique veniam, id, iure hic accusamus molestias explicabo animi.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis qui perferendis at possimus alias inventore sint numquam, doloribus iste veniam accusamus mollitia repellendus nobis ipsa quae esse ex sapiente exercitationem ad! Repudiandae sapiente sequi labore hic ipsam tempora fugiat quia distinctio illum aut soluta, excepturi amet expedita facere quae quod?</p>
+    <button class="bot get">Get Started </button>
+  </div>
+  <img src="toolm5.1.webp" />
+ 
+</div>
+
+<div class="cards">
+  <div class="card black">
+    <h2>Create Unique Digital Art</h2>
+    <img class="photo" src="toolm5.2.jpg"/>
+  </div>
+  <div class="card green">
+    <h2>Professional tools for illustration design...</h2>
+     <img class="img" src="toolm5.3.webp"/>
+    <button class="small-btn">Instructions </button>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
